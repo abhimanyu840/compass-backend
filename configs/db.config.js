8af32f6c1d68e02561e5d5772ba8abbfefb04c99
abhimanyu840 <akabhimanyukumar111@gmail.com>
2024-03-20 11:15:32 +0530

    fix commit 2

--- configs/db.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    DB_NAME: "compass_db",
-    // MONGO_URI: 'mongodb://localhost:27017/compass_db'
-    MONGO_URI: 'mongodb+srv://akabhimanyukumar111:compassbackend@compassdb.njf8dtl.mongodb.net/?retryWrites=true&w=majority'
-}
